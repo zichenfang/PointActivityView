199886b9308aa7fd3470ffcf4f63073fbd7f174a
iCocoder <iCocoder@iCocoderdeMacBook-Air.local>
2014-09-03 14:21:06 +0800

    Initial Commit

---
+++ testForGame/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testForGame
+//
+//  Created by iCocoder on 14-9-3.
+//  Copyright (c) 2014年 24so. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ testForGame/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  testForGame
+//
+//  Created by iCocoder on 14-9-3.
+//  Copyright (c) 2014年 24so. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
